494a2c46bc2840efd8d0405b9bb84f04fbcb6305
Radhe1912 <106031414+Radhe1912@users.noreply.github.com>
2025-01-25 00:55:43 +0530

    Initialize frontend repository

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const connectToMongoDb = require('./config/db');
 connectToMongoDb();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 // apply middleware
 app.use(express.json());
